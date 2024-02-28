894584eca9a76313e75c8b2c9c86df67cf8a95f2
prathamlkamath <134483132+prathamlkamath@users.noreply.github.com>
2024-02-28 07:36:28 +0530

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,172 @@
+const languages = [
+  {
+    no: "1",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  
+  {
+    no: "2",
+    name: "Kannada",
+    native: "ಕನ್ನಡ",
+    code: "kn",
+},
+{
+  no: "5",
+  name: "Marathi",
+  native: "मराठी",
+  code: "mr"
+},
+  {
+    no: "3",
+    name: "Arabic",
+    native: "عربي",
+    code: "ar",
+  },
+  {
+    no: "4",
+    name: "Bengali",
+    native: "বাংলা",
+    code: "bn"
+},
+ 
+{
+  no: "6",
+  name: "Tamil",
+  native: "தமிழ்",
+  code: "ta"
+},
+{
+  no: "7",
+  name: "Urdu",
+  native: "اُردُو",
+  code: "ur",
+},
+{
+  no: "8",
+  name: "Gujarati",
+  native: "ગુજરાતી",
+  code: "gu"
+},
+{
+  no: "9",
+  name: "Odia",
+  native: "ଓଡ଼ିଆ",
+  code: "or"
+},
+{
+  no: "10",
+  name: "Malayalam",
+  native: "മലയാളം",
+  code: "ml"
+},
+{
+  no: "14",
+  name: "Sanskrit",
+  native: "संस्कृतम्",
+  code: "sa"
+},
+
+
+  
+ 
+ 
+  
+  {
+    no: "10",
+    name: "Chinese (Simplified)",
+    native: "中文简体",
+    code: "zh-CN",
+  },
+  {
+    no: "11",
+    name: "Chinese (Traditional)",
+    native: "中文繁體",
+    code: "zh-TW",
+  },
+  
+ 
+  
+ 
+  
+  
+  
+  {
+    no: "20",
+    name: "French",
+    native: "Français",
+    code: "fr",
+  },
+  
+  
+  {
+    no: "23",
+    name: "German",
+    native: "Deutsch",
+    code: "de",
+  },
+  {
+    no: "24",
+    name: "Greek",
+    native: "Ελληνικά",
+    code: "el",
+  },
+  
+  
+  {
+    no: "27",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  {
+    no: "28",
+    name: "Hungarian",
+    native: "Magyar",
+    code: "hu",
+  },
+ 
+  
+  
+  {
+    no: "32",
+    name: "Italian",
+    native: "Italiano",
+    code: "it",
+  },
+  {
+    no: "33",
+    name: "Japanese",
+    native: "日本語",
+    code: "ja",
+  },
+  {
+    no: "34",
+    name: "Korean",
+    native: "한국어",
+    code: "ko",
+  },
+  {
+    no: "35",
+    name: "Latvian",
+    native: "Latviešu",
+    code: "lv",
+  },
+  {
+    no: "41",
+    name: "Persian",
+    native: "فارسی",
+    code: "fa",
+  },
+ 
+  
+  
+  
+  
+
+ 
+  
+  
+ 
+];
